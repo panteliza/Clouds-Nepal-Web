2a1ffbb7dcaf1819a0ab32d435f5a25d2b824f06
Eliza Pant <elizapant.github@gmail.com>
2025-07-19 20:13:00 +0545

    qwdw

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,7 @@ import FloatingButtons from './components/FloatingButtons';
 
 import Home from './pages/Home';
 import CoursesPage from './pages/CoursesPage';
-import DigitalMarketing from './pages/DigitalMarketing';
-import PayPerClick from './pages/PayPerClick';
-import ContentWriting from './pages/ContentWriting';
-import GraphicDesign from './pages/GraphicDesign';
-import SocialMediaMarketing from './pages/SocialMediaMarketing';
-import Seo from './pages/Seo';
-import UIUX from './pages/UIUX';
-import SystemSoftware from './pages/SystemSoftware';
-import AppDevelopment from './pages/AppDevelopment';
-import WebsiteDevelopment from './pages/WebsiteDevelopment';
+
 import FrontendDevelopmentCourse from './pages/FrontendDevelopmentCourse';
 import BackendDevelopmentCourse from './pages/BackendDevelopmentCourse';
 import FullStackDevelopmentCourse from './pages/FullStackDevelopmentCourse';
--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -2,11 +2,14 @@
 import NavbarMain from '../components/Navbar'
 
 import Courses from '../components/Courses'
+import CourseSearchBar from '../components/CourseSearchBar'
+import { allCourses } from "../components/courses";
 
 const CoursesPage = () => {
   return (
      <div className="pt-[70px]">
       <NavbarMain/>
+       <CourseSearchBar allCourses={allCourses} />
       <Courses/>
     </div>
   )
