8fdbc61947a1a578441c1c6eb6113c699db9f0ac
Eliza Pant <elizapant.github@gmail.com>
2025-07-24 18:33:30 +0545

    guyyuuy

--- src/pages/OurMission.jsx
+++ src/pages/OurMission.jsx
@@ -69,7 +69,7 @@ const MissionPage = () => {
         </div>
 
         <motion.div
-          className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10"
+          className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10"
           initial="hidden"
           animate="visible"
           variants={{
